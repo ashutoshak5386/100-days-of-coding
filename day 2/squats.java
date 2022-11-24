cfafc65e23a6a2867727a930166a536b4b0afe95
ashutoshak5386 <50767646+ashutoshak5386@users.noreply.github.com>
2022-11-24 07:57:21 +0530

    Add files via upload

---
+++ day 2/squats.java
@@ -0,0 +1,31 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+       int i,t,x;
+        Scanner sc = new Scanner(System.in);
+        t = sc.nextInt();
+        
+        for(i=0; i<t; i++){
+            
+            x = sc.nextInt();
+       
+            System.out.println(x*15);
+            
+    
+        
+        }
+       
+        sc.close();
+        
+		
+	}
+}
